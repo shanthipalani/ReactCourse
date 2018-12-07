52d98dc40076e6d9e3b73dc11ee5c0f49415268a
shpalani <shpalani@paypal.com>
2018-12-07 09:53:19 +0530

    formElements

--- main.js
+++ main.js
@@ -1,14 +1,5 @@
-/*import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './findDOMNode.jsx';
-
-ReactDOM.render(<App />, document.getElementById('container'));*/
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './lifeCycle.jsx';
+import App from './event.jsx';
 
-ReactDOM.render(<App/>, document.getElementById('app'));
-
-setTimeout(() => {
-    ReactDOM.unmountComponentAtNode(document.getElementById('app'));}, 10000);
+ReactDOM.render(<App />, document.getElementById('app'));
