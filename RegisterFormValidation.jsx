e78370f204c18be59adb54804e247c62b18b8430
shpalani <shpalani@paypal.com>
2018-12-11 09:23:21 +0530

    router

--- RegisterFormValidation.jsx
+++ RegisterFormValidation.jsx
@@ -16,9 +16,12 @@ class App extends React.Component {
 
     handleChange(e) {
         let fields = this.state.fields;
+        let errors = this.state.errors;
         fields[e.target.name] = e.target.value;
+        errors[e.target.name] = '';
         this.setState({
-            fields
+            fields:fields ,
+            errors: errors
         });
 
     }
@@ -110,15 +113,19 @@ class App extends React.Component {
                         <label>Name</label>
                         <input type="text" name="username" value={this.state.fields.username} onChange={this.handleChange} />
                         <div className="errorMsg">{this.state.errors.username}</div>
+
                         <label>Email ID:</label>
                         <input type="text" name="emailid" value={this.state.fields.emailid} onChange={this.handleChange}  />
                         <div className="errorMsg">{this.state.errors.emailid}</div>
+
                         <label>Mobile No:</label>
                         <input type="text" name="mobileno" value={this.state.fields.mobileno} onChange={this.handleChange}   />
                         <div className="errorMsg">{this.state.errors.mobileno}</div>
+
                         <label>Password</label>
                         <input type="password" name="password" value={this.state.fields.password} onChange={this.handleChange} />
                         <div className="errorMsg">{this.state.errors.password}</div>
+
                         <input type="submit" className="button"  value="Register"/>
                     </form>
                 </div>
